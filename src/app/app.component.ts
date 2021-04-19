ea974d6628e5261f4835f6756d6565754d1000e5
gutembergg <gmascarenhas3001@gmail.com>
2021-04-19 18:43:52 +0200

    create swUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
-import { Plugins } from '@capacitor/core';
+import { SwUpdate } from "@angular/service-worker";
+import { Plugins } from "@capacitor/core";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-root",
@@ -8,4 +10,42 @@ import { Plugins } from '@capacitor/core';
 })
 export class AppComponent {
   title = "academicus";
+
+  constructor(private _toast: ToastController, private _sw: SwUpdate) {
+    this.checkDevice();
+  }
+
+  checkDevice() {
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test(userAgent);
+    };
+
+    // Detect if app is install
+    const isInStandaloneMode = () =>
+      "standalone" in (window as any).navigator &&
+      (window as any).navigator.standalone;
+    if (isIos() && !isInStandaloneMode()) {
+      this.displayPopUpInstall();
+    }
+  }
+
+  async displayPopUpInstall() {
+    const toast = await this._toast.create({
+      message: `Pour installer l'application...`,
+      position: "bottom",
+      keyboardClose: true,
+      color: "dark",
+      buttons: [
+        {
+          text: "ok",
+          role: "cancel",
+          handler: () => {
+            console.log("pppppppp");
+          }
+        }
+      ]
+    });
+    await toast.present();
+  }
 }
