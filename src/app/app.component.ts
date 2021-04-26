89cf97e1256e39514995193f3258ae729c6aaa80
gutembergg <gmascarenhas3001@gmail.com>
2021-04-26 09:10:42 +0200

    add available update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent {
 
   constructor(private _toast: ToastController, private _sw: SwUpdate) {
     this.checkDevice();
+    this._sw.available.subscribe((response) => {
+      if (response) {
+        this.displayPopUpUpdate();
+      }
+    });
   }
 
   checkDevice() {
@@ -42,6 +47,26 @@ export class AppComponent {
           role: "cancel",
           handler: () => {
             console.log("pppppppp");
+          }
+        }
+      ]
+    });
+    await toast.present();
+  }
+
+  async displayPopUpUpdate() {
+    const toast = await this._toast.create({
+      message: `Vous avez une nouvelle version d'Academicus`,
+      position: "bottom",
+      keyboardClose: true,
+      color: "dark",
+      buttons: [
+        {
+          text: "ok",
+          role: "cancel",
+          handler: async () => {
+            this._sw.activateUpdate();
+            window.location.reload();
           }
         }
       ]
